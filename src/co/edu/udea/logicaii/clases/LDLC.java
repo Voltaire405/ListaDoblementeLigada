4930487fc53c686ece80c61eb3df9df9c91a4014
Voltaire405 <jlma.physics@gmail.com>
2019-09-10 00:21:09 -0500

    v.0.2 funcional para pruebas

--- src/co/edu/udea/logicaii/clases/LDLC.java
+++ src/co/edu/udea/logicaii/clases/LDLC.java
@@ -5,37 +5,38 @@ import javax.swing.JOptionPane;
 public class LDLC {
 	private NodoDoble primero, ultimo;
 	//private boolean finDeRecorrido;
-	
+
 	public LDLC() {			//constructor
 		primero=null;
 		ultimo= null;
 		//finDeRecorrido=false;
 	}
-	
+
 	public boolean esVacia() {
 		return primero==null;
 	}
-	
+
 	public NodoDoble primerNodo() {
 		return primero;
 	}
-	
+
 	public NodoDoble ultimoNodo(){
 		return ultimo;
 	}
-	
+
 	public boolean finDeRecorrido(NodoDoble p) {
 		return p==primero;
 	}
-	
+
 	public void mostrarLista() {
 		NodoDoble p=primerNodo();
 		String dataToPrint ="";
-		
-		if(p==null) {			//if para verificar si la lista est� vacia			
+
+		if(p==null) {   //if para verificar si la lista est� vacia
+            JOptionPane.showMessageDialog(null, dataToPrint);
 			return;
 		}
-		
+
 		do {
 			dataToPrint += "[" + p.getDato() + "] ";
 			p=p.getLd();
@@ -43,7 +44,7 @@ public class LDLC {
 		while(p!= primero);
 		JOptionPane.showMessageDialog(null, dataToPrint);
 	}
-	
+
 	public NodoDoble anterior(NodoDoble x) {
 		NodoDoble p, y;
 		p=primerNodo();
@@ -54,38 +55,38 @@ public class LDLC {
 		}
 		return y;
 	}
-	
+
 	public NodoDoble buscaDondeInsertar(int d) {
 		NodoDoble p, y;
 		p=primerNodo();
 		y=anterior(p);				//ultimoNodo()
-		
+
 		//caso en el que la lista este vacia
-		if(p==null) {					
+		if(p==null) {
 			return y;
 		}
-		
+
 		//caso en el que el primer nodo contenga un dato mayor
 		//al parametro d
 		if(p.getDato()> d) {
 			return y;
 		}
-		
+
 		//uso de do while puesto que la condicion para finDeRecorrido es
 		//p==primero
-		do {			
+		do {
 			y=p;
 			p=p.getLd();
 		}while(!finDeRecorrido(p) && p.getDato()<d);
-		return y;		
+		return y;
 	}
-	
+
 	public void insertar(int d, NodoDoble y) {
 		NodoDoble x;
 		x= new NodoDoble(d);
 		conectar(x, y);
 	}
-	
+
 	public void conectar(NodoDoble x, NodoDoble y) {
 		if(y==null) {				//caso de lista vacia
 			x.setLd(x);
@@ -93,13 +94,13 @@ public class LDLC {
 			primero=x;
 			ultimo=x;
 		}
-		
+
 		else {
 			x.setLi(y);
 			x.setLd(y.getLd());
 			y.setLd(x);
 			x.getLd().setLi(x);
-			
+
 			if(y==ultimo) {
 				//Puesto que es una lista circular, si el nodo "y" es
 				//igual al ultimo nodo, exiten dos posibilidades:
@@ -115,33 +116,33 @@ public class LDLC {
 			}
 		}
 	}
-	
+
 	public NodoDoble buscarDato(int d) {
 		NodoDoble x= primerNodo();
-		
+
 		if(x==null) {				//caso lista vacia
 			return x;
 		}
-		
+
 		if(x.getDato()==d) {				//dato primer nodo == d
 			return x;
 		}
-		
+
 		do{
 			x=x.getLd();
 		}while(!finDeRecorrido(x) && x.getDato()!= d);
 		return x;
 	}
-	
+
 	public void borrar(NodoDoble x, int d) {
 		if(x==null || x.getDato()!= d) {
 			JOptionPane.showMessageDialog(null, "El dato a borrar no existe");
 			return;
 		}
-		
+
 		desconectar(x);
 	}
-	
+
 	public void desconectar(NodoDoble x) {
 		if(primero.getLd()==primero) {	 //caso solo un nodo en la lista
 			primero=null;
@@ -157,9 +158,9 @@ public class LDLC {
 				ultimo=x.getLi();
 			}
 		}
-		
+
 	}
-	
+
 	public void ordenarDescendentemente() {
 		NodoDoble p, ap, mayor, aMayor, q, aq;
 		if(primero==null) {
@@ -201,7 +202,7 @@ public class LDLC {
 			}
 		}
 	}
-	
+
 	public void intercambiarExtremos() {
 		NodoDoble x=primerNodo();
 		desconectar(x);
@@ -210,7 +211,7 @@ public class LDLC {
 		ultimo= x;
 
 	}
-	
+
 	public void eliminarLista() {
 		NodoDoble x= primerNodo();
 		if(x==null) {
@@ -238,10 +239,12 @@ public class LDLC {
 			break;
 		}
 	}
-	
+
 	public void insertarAlFinal(int d) {
 		NodoDoble x= new NodoDoble(d);
 		if(primero==null) {
+            x.setLd(x);
+            x.setLi(x);
 			primero=x;
 			ultimo=x;
 		}
@@ -252,12 +255,14 @@ public class LDLC {
 			primero.setLi(x);
 			ultimo=x;
 		}
-		
+
 	}
 
 	public void insertarAlPrincipio(int d) {
 		NodoDoble x= new NodoDoble(d);
 		if(primero==null) {
+            x.setLd(x);
+            x.setLi(x);
 			primero=x;
 			ultimo=x;
 		}
--- src/co/edu/udea/logicaii/principal/Programa.java
+++ src/co/edu/udea/logicaii/principal/Programa.java
@@ -3,7 +3,9 @@ package co.edu.udea.logicaii.principal;
 import co.edu.udea.logicaii.clases.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.text.ParseException;
+import java.util.Optional;
 
 public class Programa {
     public static void main(String[] args) {
@@ -63,6 +65,11 @@ public class Programa {
 
     //Evalua la opcion ingresada por el usuario
     private static void evalOptionMainMenu(String opt, LDLC lista) {
+        NodoDoble y = null;
+        NodoDoble x = null;
+        Integer userEntry = null;
+        Integer userEntry2 = null;
+
         try {
             switch (opt) {
                 case "1":
@@ -71,8 +78,185 @@ public class Programa {
                 case "2":
                     crearActualizarLista(lista, opt);
                     break;
+                case "3":
+                    if (lista.esVacia())
+                        JOptionPane.showMessageDialog(null,"Lista vacía!");
+                    else
+                        JOptionPane.showMessageDialog(null,"No está vacía!");
+                    break;
                 case "7":
                     lista.mostrarLista();
+                    break;
+                case "4":
+                    try {
+                        JOptionPane.showMessageDialog(null, lista.primerNodo().getDato());
+                    } catch (NullPointerException e) {
+                        JOptionPane.showMessageDialog(null, "Dato nulo!");
+                    }
+                    break;
+                case "5":
+                    try {
+                        JOptionPane.showMessageDialog(null, lista.ultimoNodo().getDato());
+                    } catch (NullPointerException e) {
+                        JOptionPane.showMessageDialog(null, "Dato nulo!");
+                    }
+                    break;
+                case "6":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        y = lista.buscarDato(userEntry);
+                        try {
+                            if (lista.finDeRecorrido(y))
+                                JOptionPane.showMessageDialog(null, "Verdadero!");
+                            else JOptionPane.showMessageDialog(null, "Falso!");
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Verdadero!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+
+                    break;
+                case "8":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        y = lista.buscarDato(userEntry);
+                        try {
+                            y = lista.anterior(y);
+                            JOptionPane.showMessageDialog(null, y.getDato());
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+
+                    break;
+                case "9":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        try {
+                            y = lista.buscaDondeInsertar(userEntry);
+                            JOptionPane.showMessageDialog(null, y.getDato());
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+
+                    break;
+                case "10":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        userEntry2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato de un nodo de la lista"));
+                        y = lista.buscarDato(userEntry2);
+                        try {
+                            if (y == null) {
+                                if (lista.esVacia()) {
+                                    lista.insertar(userEntry, y);
+                                    JOptionPane.showMessageDialog(null, "Dato insertado!");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "El dato ingresado no existe" +
+                                            ", consulte los datos de la lista previamente.");
+                                }
+                            } else {
+                                lista.insertar(userEntry, y);
+                                JOptionPane.showMessageDialog(null, "Dato insertado!");
+                            }
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+
+                    break;
+                case "11":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        userEntry2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato de un nodo de la lista " +
+                                "a continuación del cual conectar el nuevo nodo."));
+                        y = lista.buscarDato(userEntry2);
+                        try {
+                            x = new NodoDoble(userEntry);
+                            if (y == null) {
+                                if (lista.esVacia()) {
+                                    lista.conectar(x, y);
+                                    JOptionPane.showMessageDialog(null, "Dato conectado!");
+                                } else {
+                                    JOptionPane.showMessageDialog(null, "El dato ingresado no existe" +
+                                            ", consulte los datos de la lista previamente.");
+                                }
+                            } else {
+                                lista.conectar(x, y);
+                                JOptionPane.showMessageDialog(null, "Dato conectado!");
+                            }
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+                    break;
+                case "12":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        try {
+                            y = lista.buscarDato(userEntry);
+                            JOptionPane.showMessageDialog(null, y.getDato());
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "No existe el dato en la lista!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+                    break;
+                case "13":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato a borrar"));
+                        try {
+                            y = lista.buscarDato(userEntry);
+                            lista.borrar(y, userEntry);
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+                    break;
+                case "14":
+                    try {
+                        userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
+                        try {
+                            y = lista.buscarDato(userEntry);
+                            lista.desconectar(y);
+                            JOptionPane.showMessageDialog(null, "Nodo desconectado");
+                        } catch (NullPointerException e) {
+                            JOptionPane.showMessageDialog(null, "Dato Nulo!");
+                        }
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Número no válido");
+                    }
+                    break;
+                case "16":
+                    try {
+                        JOptionPane.showMessageDialog(null, "primero: " + lista.primerNodo().getDato() + "" +
+                                "- ultimo: " + lista.ultimoNodo().getDato());
+                        lista.intercambiarExtremos();
+                        JOptionPane.showMessageDialog(null, "primero: " + lista.primerNodo().getDato() + "" +
+                                "- ultimo: " + lista.ultimoNodo().getDato());
+                    } catch (NullPointerException e) {
+                        JOptionPane.showMessageDialog(null, "Lista vacía!");
+                    }
+                    break;
+                case "15":
+                    lista.ordenarDescendentemente();
+                    lista.mostrarLista();
+                    break;
+                case "17":
+                    lista.eliminarLista();
+                    JOptionPane.showMessageDialog(null,"Lista borrada!");
                     break;
 
                 default:
@@ -101,7 +285,7 @@ public class Programa {
                 break;
             evalOptionCrearActualizarLista(option, lista, source);
 
-        } while ("1".equals(option)||"2".equals(option)||"3".equals(option));
+        } while ("1".equals(option) || "2".equals(option) || "3".equals(option));
 
 
     }
@@ -110,8 +294,10 @@ public class Programa {
     private static void evalOptionCrearActualizarLista(String opt, LDLC lista, String source) {
         Integer userEntry;
         String option;
-        if ("1".equals(source)){lista.eliminarLista();}    //Crear es identico a actualizar
-            //con la salvedad que crear implica limpiar la lista.
+        if ("1".equals(source)) {
+            lista.eliminarLista();
+        }    //Crear es identico a actualizar
+        //con la salvedad que crear implica limpiar la lista.
 
         switch (opt) {
             case "1":
@@ -119,7 +305,7 @@ public class Programa {
                 do {
                     try {
                         userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
-                        lista.actualizarLista(userEntry,opt);
+                        lista.actualizarLista(userEntry, opt);
                     } catch (NumberFormatException e) {
                         JOptionPane.showMessageDialog(null, "Entrada no es un entero válido!");
                     }
@@ -134,7 +320,7 @@ public class Programa {
                 do {
                     try {
                         userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
-                        lista.actualizarLista(userEntry,opt);
+                        lista.actualizarLista(userEntry, opt);
                     } catch (NumberFormatException e) {
                         JOptionPane.showMessageDialog(null, "Entrada no es un entero válido!");
                     }
@@ -148,7 +334,7 @@ public class Programa {
                 do {
                     try {
                         userEntry = Integer.parseInt(JOptionPane.showInputDialog("Ingrese por favor un entero como dato"));
-                        lista.actualizarLista(userEntry,opt);
+                        lista.actualizarLista(userEntry, opt);
                     } catch (NumberFormatException e) {
                         JOptionPane.showMessageDialog(null, "Entrada no es un entero válido!");
                     }
